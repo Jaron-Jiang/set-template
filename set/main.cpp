4ccd2a00ac8de85417d94553bd54382ca8ab654c
Jaron-Jiang <2896247746@qq.com>
2020-10-08 14:03:19 +0800

    添加了命令行选择本次运行使用的集合的数据类型

--- set/main.cpp
+++ set/main.cpp
@@ -3,7 +3,11 @@
 #include<conio.h>
 #include"set.h"
 #include"CUI.h"
-
+#include <windows.h>
+#define x1 10
+#define y1 45
+#define x2 11
+#define y2 67
 struct person
 {
 	double age;
@@ -24,6 +28,7 @@ void show(person x)
 {
 	std::cout << x.age << std::endl;
 }
+
 void print(std::string str)
 {
 	int L = (120 - str.size())/2;
@@ -33,6 +38,7 @@ void print(std::string str)
 	}
 	std::cout << str << std::endl;
 }
+
 void fun()
 {
 	for (int i = 1; i <= 120; i++)
@@ -47,9 +53,9 @@ void fun()
 	std::cout << std::endl << std::endl << std::endl;
 	str = "功能";
 	print(str);
-	str = "1. 通过使用操作命令，可以对char，int，float，double的集合，进行混合运算";
+	str = "1. 通过使用操作命令，可以对char，int，float，double的集合，进行混合表达式运算";
 	print(str);
-	str = "2. 通过代码修改，可以对任意类型进行混合运算                            ";
+	str = "2. 通过代码修改，可以对任意类型进行混合运算                                  ";
 	print(str);
 	std::cout << std::endl << std::endl << std::endl;
 	str = "操作命令";
@@ -78,13 +84,106 @@ void fun()
 	}
 	std::cout << std::endl;
 }
+
+bool ban(char val)
+{
+	if ('a' <= val && val <= 'z')
+		return true;
+	return false;
+}
+void fun1()
+{
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("\n");
+	}
+	std::string str = "请选择要使用的那种数据类型的集合";
+	print(str);
+	str = "1. int                2. float";
+	print(str);
+	str = "3. double             4. char ";
+	print(str);
+}
 int main()
 {
+	system("color F0");
 	fun();
 	_getch();
 	system("cls");
-	CUI<float>a;
-	a.run();
+	fun1();
+	int ch;
+	bool flag = true;
+	HANDLE   hCon;//定义一个句柄 
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);   //获得输出设备的句柄 
+	COORD   setps; //定义结构体变量 
+	setps.X = y1;
+	setps.Y = x1;
+	while (flag)
+	{
+		SetConsoleCursorPosition(hCon, setps);
+		ch = _getch();
+		switch (ch)
+		{
+		case 13:
+			flag = false;
+			break;
+		case 72:
+			setps.Y = (setps.Y == x1 ? x2 : x1);
+			break;
+		case 80:
+			setps.Y = (setps.Y == x1 ? x2 : x1);
+			break;
+		case 75:
+			setps.X = (setps.X == y1 ? y2 : y1);
+			break;
+		case 77:
+			setps.X = (setps.X == y1 ? y2 : y1);
+			break;
+		case 49:
+			setps.Y = x1;
+			setps.X = y1;
+			flag = false;
+			break;
+		case 50:
+			setps.Y = x1;
+			setps.X = y2;
+			flag = false;
+			break;
+		case 51:
+			setps.Y = x2;
+			setps.X = y1;
+			flag = false;
+			break;
+		case 52:
+			setps.Y = x2;
+			setps.X = y2;
+			flag = false;
+			break;
+		default:
+			break;
+		}
+	}
+	system("cls");
+	if (setps.Y == x1 && setps.X == y1)
+	{
+		CUI<int>a;
+		a.run();
+	}
+	else if (setps.Y == x1 && setps.X == y2)
+	{
+		CUI<float>a;
+		a.run();
+	}
+	else if (setps.Y == x2 && setps.X == y1)
+	{
+		CUI<double>a;
+		a.run();
+	}
+	else if (setps.Y == x2 && setps.X == y2)
+	{
+		CUI<char>a(ban);
+		a.run();
+	}
 	/*int x, y;
 	std::cin >> x >> y;
 	system("cls");
