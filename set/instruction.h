b636a264f2514df619ec4e76c63dc2897b54eb57
Jaron-Jiang <2896247746@qq.com>
2020-10-08 19:55:33 +0800

    添加find和subset命令的说明

--- set/instruction.h
+++ set/instruction.h
@@ -13,6 +13,8 @@ private:
 	void _insert();
 	void _ls();
 	void _delete();
+	void _find();
+	void _subset();
 public:
 	void run();
 };
@@ -21,46 +23,66 @@ void instruction::_clean()
 {
 	printf("使用方法:\n\t直接输入clean(),删除定义的所有集合，慎用！！！\n");
 }
+
 void instruction::_assign()
 {
 	printf("使用方法:\n\t输入assign name，声明一个集合变量name\n\t注意:这里的name为要定义的集合变量名，此外一次只能定义一个变量,如果该变量已经存在，则覆盖\n");
 }
+
 void instruction::_show()
 {
 	printf("使用方法:\n\t1. 输入show --all，显示所有定义但未删除的集合的名字和集合元素\n\t2. 输入show name，显示集合name的所有元素\n\t注意:这里的name为集合变量名，此外一次只能写一个变量名\n");
 }
+
 void instruction::_erase()
 {
 	printf("使用方法:\n\t输入erase name val，删除集合name中的元素val\n\t注意:这里的name为集合变量名，此外一次只能删除一个元素\n");
 }
+
 void instruction::_size()
 {
 	printf("使用方法:\n\t输入size name，显示集合name的元素个数\n");
 }
+
 void instruction::_quit()
 {
 	printf("使用方法:\n\t输入quit()，程序结束运行\n");
 }
+
 void instruction::_com()
 {
 	printf("使用方法:\n\t输入com <表达式>，运算表达式，如果该表达不是赋值运算，则会输出，反之则不会输出\n");
 	printf("\t例如:a+b-c这个表达式为非赋值表达式；a=b+(c-d)+d[p]为赋值表达式\n");
 	printf("\t符号+表示并集运算，符号-表示差集运算，符号^表示交集运算，符号[]表示补集运算(例如A[B]表示全集B下A的补集)，可以使用符号()进行优先级运算\n");
 }
+
 void instruction::_insert()
 {
 	printf("使用方法:\n\t输入insert name val，向集合name中加入元素val\n");
 	printf("注意:\n\t1. 如果集合是char，则将val中的字符单个加入集合\n");
 	printf("\t2. 如果集合是整数或者浮点数类型，则将val转换成数字加入集合，同时可以进行insert name val1 val2 val3...valn,即将val1，val2，val3...valn分别加入集合\n");
 }
+
 void instruction::_ls()
 {
 	printf("使用方法:\n\t输入ls,显示所有定义且未删除的变量名和集合中的元素个数\n");
 }
+
 void instruction::_delete()
 {
 	printf("使用方法:\n\t输入delete name,删除变量name，name为定义的一个变量名\n");
 }
+
+void instruction::_find()
+{
+	printf("使用方法:\n\t输入find name val,查看集合name中是否存在val，存在输出True，反之输出False\n");
+}
+
+void instruction::_subset()
+{
+	printf("使用方法:\n\t输入subset name1 name2,查看集合name1是否是name2的子集，是输出True，不是输出False\n");
+}
+
 void instruction::run()
 {
 	char command[100];
@@ -87,6 +109,10 @@ void instruction::run()
 		_ls();
 		printf("\ndelete:\n\n");
 		_delete();
+		printf("\nfind:\n\n");
+		_find();
+		printf("\nsubset:\n\n");
+		_subset();
 	}
 	else if(strcmp(command, "-clean") == 0)
 	{
@@ -127,6 +153,14 @@ void instruction::run()
 	else if (strcmp(command, "-delete") == 0)
 	{
 		_delete();
+	}
+	else if (strcmp(command, "-find") == 0)
+	{
+		_find();
+	}
+	else if (strcmp(command, "-subset") == 0)
+	{
+		_subset();
 	}
 	else 
 	{
